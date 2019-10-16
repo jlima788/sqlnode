2c367155a23287bb4533a48d30eb32f4da7fb803
Everaldo Lima <jemlima@gmail.com>
2019-10-16 18:00:52 -0300

    First Commit

---
+++ src/config/database.js
@@ -0,0 +1,11 @@
+module.exports = {
+  dialect: "postgres",
+  host: "localhost",
+  username: "default",
+  password: "secret",
+  database: "sqlnode",
+  define: {
+    timestamps: true,
+    underscored: true
+  }
+};
